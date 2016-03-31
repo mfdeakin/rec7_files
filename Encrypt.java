b7cf2a389ce6f9c3459cb0f813cc5cb6352a75e9
Michael Deakin <mfdeakin@cs.unc.edu>
2016-03-31 16:14:45 -0400

    Finished the OTP encryption program

--- Encrypt.java
+++ Encrypt.java
@@ -5,54 +5,54 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class Encrypt extends InputStream {
-    private InputStream text, key;
-    public Encrypt(InputStream text, InputStream key) {
-	this.text = text;
-	this.key = key;
-    }
-
-    public int available() throws IOException {
-	int textAvail = text.available();
-	int keyAvail = key.available();
-	if(textAvail < keyAvail) {
-	    return textAvail;
+	private InputStream text, key;
+	public Encrypt(InputStream text, InputStream key) {
+		this.text = text;
+		this.key = key;
 	}
-	else {
-	    return keyAvail;
+
+	public int available() throws IOException {
+		int textAvail = text.available();
+		int keyAvail = key.available();
+		if(textAvail < keyAvail) {
+			return textAvail;
+		}
+		else {
+			return keyAvail;
+		}
 	}
-    }
 
-    public void close() throws IOException {
-	text.close();
-	key.close();
-    }
-
-    public boolean markSupported() {
-	return text.markSupported() && key.markSupported();
-    }
-
-    public void mark(int readLimit) {
-	text.mark(readLimit);
-	key.mark(readLimit);
-    }
-
-    public void reset() throws IOException {
-	text.reset();
-	key.reset();
-    }
-
-    public int read() throws IOException {
-	int textVal = text.read();
-	int keyVal = key.read();
-	return textVal ^ keyVal;
-    }
-
-    public long skip(long n) throws IOException{
-	long tskip = text.skip(n);
-	long kskip = key.skip(n);
-	if(tskip != kskip) {
-	    throw new IOException("Could not skip the text and key by the same amount");
+	public void close() throws IOException {
+		text.close();
+		key.close();
 	}
-	return tskip;
-    }
+
+	public boolean markSupported() {
+		return text.markSupported() && key.markSupported();
+	}
+
+	public void mark(int readLimit) {
+		text.mark(readLimit);
+		key.mark(readLimit);
+	}
+
+	public void reset() throws IOException {
+		text.reset();
+		key.reset();
+	}
+
+	public int read() throws IOException {
+		int textVal = text.read();
+		int keyVal = key.read();
+		return textVal ^ keyVal;
+	}
+
+	public long skip(long n) throws IOException{
+		long tskip = text.skip(n);
+		long kskip = key.skip(n);
+		if(tskip != kskip) {
+			throw new IOException("Could not skip the text and key by the same amount");
+		}
+		return tskip;
+	}
 }
--- OneTimePad.java
+++ OneTimePad.java
@@ -3,17 +3,27 @@ package rec8_files;
 
 import java.util.Scanner;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class OneTimePad {
     
-    public static void main(String[] args) {
-	System.out.println("Text File to encrypt/decrypt:");
-	Scanner input = new Scanner(System.in);
-	String textFName = input.next().trim();
-	FileInputStream text = new FileInputStream(textFName);
-	System.out.println("Key File to use to encrypt/decrypt:");
-	String keyFName = input.next().trim();
-	FileInputStream key = new FileInputStream(keyFName);
-	Encrypt encrypted = new Encrypt(text, key);
+  public static void main(String[] args) throws IOException {
+    System.out.println("Text File to encrypt/decrypt:");
+    Scanner input = new Scanner(System.in);
+    String textFName = input.next().trim();
+    FileInputStream text = new FileInputStream(textFName);
+    System.out.println("Key File to use to encrypt/decrypt:");
+    String keyFName = input.next().trim();
+    FileInputStream key = new FileInputStream(keyFName);
+    Encrypt encrypted = new Encrypt(text, key);
+    System.out.println("Output file:");
+    String outFName = input.next().trim();
+    FileOutputStream out = new FileOutputStream(outFName);
+    while(encrypted.available() > 0) {
+    	out.write(encrypted.read());
     }
+    input.close();
+    encrypted.close();
+  }
 }
